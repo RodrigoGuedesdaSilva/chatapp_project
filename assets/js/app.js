0d46f0707b8eb280f94d8218d6957a84a8e3505e
Rodrigo Guedes da Silva <79825051+RodrigoGuedesdaSilva@users.noreply.github.com>
2021-09-24 14:09:04 -0300

    Commented on the onclick function

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,10 +5,10 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('active');
     menuLink.classList.toggle('is-active');
 })
-
+/*
 document.onclick = function(e) {
     if(e.target !== menu && e.target !== menuLink) {
         menu.classList.remove('active');
         menuLink.classList.remove('is-active')
     }
-}
+}*/
